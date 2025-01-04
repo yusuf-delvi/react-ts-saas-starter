1972d57e5ee907c5f01f141d19252b3f833b3b7b
yusuf-delvi <yusufmddelvi8@gmail.com>
2025-01-04 17:37:29 +0530

    use google api from env var

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import App from './App.tsx';
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const googleClientId = import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID;
+
+if (!googleClientId) {
+	throw new Error('Google Client ID is not defined in environment variables');
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	// Google client id is required
-	<GoogleOAuthProvider clientId='google-client-id'>
+	<GoogleOAuthProvider clientId={googleClientId}>
 		<App />
 	</GoogleOAuthProvider>
 );
