6a08e03cc68edc42189e3f9f7593e6242c27e158
yusuf-delvi <yusufmddelvi8@gmail.com>
2025-01-04 17:39:53 +0530

    set default port

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	plugins: [react(), EnvironmentPlugin('all'), tsconfigPaths()],
+	server: {
+		port: 3001,
+	},
 });
